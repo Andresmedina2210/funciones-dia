a762332ba289b7f8abe588a3d78b6549f02fe499
Andres Medina <am9226605@gmail.com>
2024-10-03 14:52:12 -0500

    Se crea el archivo main y una funcion con el hola mundo

---
+++ funcionesProject/main.py
@@ -0,0 +1,11 @@
+# Definicion de una funcion
+def my_function():
+    print("Hello from a function")
+# Invocar la funcion
+my_function()
+
+def mastrarNombre(nombre):
+    print("Su nombre es: "+nombre)
+
+nom = input("Ingrese su nombre: ")
+mastrarNombre(nom)
